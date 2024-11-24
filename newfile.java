3ee4c39dfb5741e36d337c5a95011996ed14480b
jkirankumar93 <jkirankumar9848@gmail.com>
2024-11-24 12:28:13 +0530

    lksjdf

--- newfile.java
+++ newfile.java
@@ -7,3 +7,19 @@ blic static void moreParameters(int a, int b)
 		System.out.println("in method moreParameters. a: " + a + " b: " + b);	
 	}
 }
+	public static void go()
+	{	int x = 3;
+		int y = 2;
+		System.out.println("In method go. x: " + x + " y: " + y);
+		falseSwap(x,y);
+		System.out.println("in method go. x: " + x + " y: " + y);
+		moreParameters(x,y);
+		System.out.println("in method go. x: " + x + " y: " + y);
+	}
+	
+	public static void falseSwap(int x, int y)
+	{	System.out.println("in method falseSwap. x: " + x + " y: " + y);
+		int temp = x;
+		x = y;
+		y = temp;
+		
