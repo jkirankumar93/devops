e9fa2051874623bdbc55f16d463319d91f815713
jkirankumar93 <jkirankumar9848@gmail.com>
2024-11-24 12:24:09 +0530

    new file

---
+++ newfile.java
@@ -0,0 +1,9 @@
+blic static void moreParameters(int a, int b)
+	{	System.out.println("in method moreParameters. a: " + a + " b: " + b);
+		a = a * b;
+		b = 12;
+		System.out.println("in method moreParameters. a: " + a + " b: " + b);
+		falseSwap(b,a);
+		System.out.println("in method moreParameters. a: " + a + " b: " + b);	
+	}
+}
